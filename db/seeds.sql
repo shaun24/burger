ab521026d58899a947f5ea54aad58d7d8da7ecc5
shazamba <Shaunshah24@gmail.com>
2018-04-02 22:14:25 -0400

    fin

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+USE `burgers_db`;
+
+
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('beef', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('veggie', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('chicken', false, CURRENT_TIMESTAMP);
